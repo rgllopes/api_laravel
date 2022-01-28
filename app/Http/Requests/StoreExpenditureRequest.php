870be187ea75c1f640f4124215432130ea7af6cc
rgllopes <rgllopes@hotmail.com>
2022-01-28 17:54:50 -0300

    Translate validator messages spanish/portuguese

--- app/Http/Requests/StoreExpenditureRequest.php
+++ app/Http/Requests/StoreExpenditureRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreExpenditureRequest extends FormRequest
@@ -14,21 +16,30 @@ class StoreExpenditureRequest extends FormRequest
     public function rules()
     {
         return [
-            'description'   => 'required|min:5',
-            'value'         => 'required|numeric',
-            'datePay'       => 'required|date',
-            'id_user'       => 'required',
+            "description"   => "required|min:5",
+            "value"         => "required|numeric",
+            "datePay"       => "required|date",
+            "id_user"       => "required",
         ];
     }
 
-    public function messages()
+    public function failedValidation(Validator $validator)
     {
-        //If show the field name use :attribute
-        return[
-            'required'  => 'Este campo é obrigatório',
-            'min'       => 'Este campo deve conter ao menos :min caracteres',
-            'numeric'   => 'Este campo deve conter somente númerops',
-            'date'      => 'Este campo deve conter uma data no formato dd/mm/yyyy'
-        ];
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
     }
+
+    // public function messages()
+    // {
+    //     //If show the field name use :attribute
+    //     return[
+    //         "required"  => "Este campo é obrigatório",
+    //         "min"       => "Este campo deve conter ao menos :min caracteres",
+    //         "numeric"   => "Este campo deve conter somente númerops",
+    //         "date"      => "Este campo deve conter uma data no formato dd mm yyyy"
+    //     ];
+    // }
 }
